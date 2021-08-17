cdd30bfea54b861e307ae2eb867aae3cfef74e7b
Carlos <contact@carloscolin.me>
2021-08-16 23:53:41 -0500

    Using directives

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template lang="pug">
   #app
-    h1 {{ msg }}
-    p {{ 1 + 1 }}
-    p {{ 'Hola' + 'Mundo' }}
-    p {{ person.name }}
-    p {{ person.name.toUpperCase() }}
-    p {{ JSON.stringify(person) }}
-    p {{ true ? 'true' : 'false' }}
+    p(v-show="showValue") {{ value }}
+
+    ul
+      li(v-for="i in items") {{ i }}
 </template>
 
 <script>
@@ -15,9 +12,10 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      person: {
-        name: 'Joe'
-      }
+      value: 'Value!',
+      showValue: true,
+
+      items: [1, 2, 3, 4]
     }
   }
 }
