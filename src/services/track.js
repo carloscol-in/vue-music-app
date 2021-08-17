312bb60149d670f83e29134035e34d93885ffe8f
Carlos <contact@carloscolin.me>
2021-08-17 11:31:36 -0500

    Created TrackService

---
+++ src/services/track.js
@@ -0,0 +1,12 @@
+import MusicService from './music-app'
+
+const TrackService = {}
+
+TrackService.search = function (q) {
+  const type = 'track'
+
+  return MusicService.get('/search', {
+    params: { q, type }
+  })
+    .then(res => res.data)
+}
