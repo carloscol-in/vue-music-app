e3dc9c70ea3b22f9d77de4563d35e318caf66671
Carlos <contact@carloscolin.me>
2021-08-17 22:36:30 -0500

    Created Notification component

---
+++ src/components/shared/Notification.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  .container
+    .columns
+      .column.is-5.is-offset-4
+        .notification.is-danger
+          slot(name="body")
+            p Something went terribly wrong!
+</template>
+
+<style lang="scss">
+  .notification {
+    margin: 10px;
+  }
+</style>
