87ab7ba8a3a192b223ec71f3365c73fd732af538
Carlos <contact@carloscolin.me>
2021-08-17 22:59:42 -0500

    Created Player component

---
+++ src/components/Player.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  .content
+    p
+      img(:src="track.album.images[0].url")
+    p
+      strong {{ track.name }}
+      small {{ track.duration }}
+    p
+      audio(controls, :src="track.preview_url")
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      track: {}
+    }
+  },
+  created () {
+    this.$bus.$on('play-track', (track) => {
+      this.track = track
+    })
+  },
+  computed: {
+    duration () {
+      return `${this.track.duration_ms}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  img {
+    width: 124px;
+    border-radius: 24px;
+  }
+</style>
