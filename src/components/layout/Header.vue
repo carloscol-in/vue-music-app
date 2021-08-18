c0663ff666a5ec22efdc9ff8866923e4578afd96
Carlos <contact@carloscolin.me>
2021-08-17 20:13:27 -0500

    Added Header component

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-head
+      header.nav
+        .container
+          .nav-left
+            .nav-item
+              strong Music App
+          .nav-right.nav-menu
+    .hero-body
+      .container.has-text-centered
+        h1.title Music App
+        h2.subtitle Now songs
+</template>
